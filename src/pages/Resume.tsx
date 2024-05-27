56d34cab090c5c45321d766f2f5d3927c078dc1d
Jemineia <choroc2020@gmail.com>
2024-05-27 21:15:04 +0900

    소개글 완성

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,18 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h1>2020</h1>
+      <p>충남대학교 입학</p>
+
+      <h1>2023</h1>
+      <p>Algorithm study 시작</p>
+      <p>백준 Sliver 1 달성</p>
+      <p>하반기 DevDay 참여</p>
+
+      <h1>2024</h1>
+      <p>상반기 실전코딩 수강</p>
+    </div>
+  );
 };
 
 export default Resume;
